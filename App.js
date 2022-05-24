4d2425f0a2c742bd62191b76b681cee311fda09b
JH <es1564@naver.com>
2022-05-24 16:39:30 +0900

    splash update

--- App.js
+++ App.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import RootStack from './src/Screens/RootStack';
 import {LogContextProvider} from './src/contexts/LogContext';
+import SplashScreen from 'react-native-splash-screen';
 
 function App() {
+  useEffect(() => {
+    try {
+      setTimeout(() => {
+        SplashScreen.hide();
+      }, 500); //스플래시 활성화 시간 2초
+    } catch (e) {
+      console.log(e.message);
+    }
+  });
+  
   return (
     <NavigationContainer>
        <LogContextProvider>
