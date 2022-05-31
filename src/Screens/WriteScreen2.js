9ef8bf88af2a5c065b28a1bafb6d82ff048fba86
JH <es1564@naver.com>
2022-05-31 15:14:05 +0900

    아이콘, 디자인 수정

--- src/Screens/WriteScreen2.js
+++ src/Screens/WriteScreen2.js
@@ -1,5 +1,5 @@
 import React,{useState} from 'react';
-import {KeyboardAvoidingView, Platform, StyleSheet,Alert,View,Text,ScrollView} from 'react-native';
+import {KeyboardAvoidingView, Platform, StyleSheet,Alert,View,Text,ScrollView, Image, ImageBackground} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import WriteHeader from '../components/WriteHeader';
 import WriteEditor from '../components/WriteEditor';
@@ -94,56 +94,67 @@ function WriteScreen2({route,saveResult}) {
             cancelable: true,
           },
         );
-      };
-  
-    return (
-      <SafeAreaView style={styles.block}>
-        <KeyboardAvoidingView
-          style={styles.avoidingView}
-          behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
-          <WriteHeader onSave={onSave} 
+  };
+
+  return (
+    <SafeAreaView style={styles.block}>
+      <KeyboardAvoidingView
+        style={styles.avoidingView}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+        <WriteHeader onSave={onSave}
           onAskRemove={onAskRemove}
           isEditing={!!log}
           date={date}
-          onChangeDate={setDate}/>
-          <ScrollView
-           horizontal={true}
-           showsHorizontalScrollIndicator = {true}
-           >
-      <View style={styles.editor}>
-      <WriteEditor
-        title={title}
-        body={body}
-        date={date}
-        onChangeTitle={setTitle}
-        onChangeBody={setBody}
-        
-      />
-      <View style={{alignItems:"flex-end",top:-20}}>
-        <Text style={{color:"white"}}>옆으로 넘기면 해설이 보여요</Text>
-        </View>
-      </View>
-          <View style={styles.tarot}>
-            {/* 뽑은 타로 알려주기 */}
-            <ScrollView style={styles.scrView}>
-            <Text>당신이 뽑은 카드는 "{tarotitle}" 입니다.</Text>
-            {/* <Text style={{color:"black"}}>{tarotitle}</Text> */}
-            <View style={styles.imgview}>{cardImg}</View>
-            {(tarottext==null)
-                    ?(
-                        <Text style={styles.txt}>
-                            명확한 답을 내리기엔 어려운 질문이군요.
-                            답은 당신의 내면이 알고 있습니다.
-                        </Text>
-                        )
-                    :(
-                        <Text style={styles.txt}>
-                            당신의 질문에 대한 대답은 '{tarottext}' 입니다. 
-                        </Text>
-                    )
-                    }
-            </ScrollView>
+          onChangeDate={setDate} />
+        <ScrollView
+          horizontal={true}
+          showsHorizontalScrollIndicator={true}
+        >
+          <View style={styles.editor}>
+            <WriteEditor
+              title={title}
+              body={body}
+              date={date}
+              onChangeTitle={setTitle}
+              onChangeBody={setBody}
+            />
+
+            <View style={{ alignItems: "flex-end", top: -20 }}>
+              <Text style={{ color: "white" }}>옆으로 넘기면 해설이 보여요</Text>
+            </View>
           </View>
+
+          
+            {/* 뽑은 타로 알려주기 */}
+            <View style={styles.tarot}>
+              <ScrollView style={styles.scrView}>
+              <Image source={require('../img/barimg.png')} style ={{alignItems:"center", marginLeft:80, marginBottom:10}}></Image>
+                <View>
+                  <Text style={{ textAlign: "center", fontSize: 19 , color:'white'}}>당신이 뽑은 카드는 {'\n'}"{tarotitle}" 입니다.</Text>
+                  {/* <Text style={{color:"black"}}>{tarotitle}</Text> */}
+                  <Image source={require('../img/bar3.png')} style={{marginTop:15}}></Image>
+
+                  <View style={styles.imgview}>{cardImg}</View>
+                  <Image source={require('../img/bar3.png')} style={{marginBottom:15}}></Image>
+
+                  {(tarottext == null)
+                    ? (
+                      <Text style={styles.txt}>
+                        명확한 답을 내리기엔 어려운 질문이군요.
+                        답은 당신의 내면이 알고 있습니다.
+                      </Text>
+                    )
+                    : (
+                      <Text style={styles.txt}>
+                        당신의 질문에 대한 대답은 '{tarottext}' 입니다.
+                      </Text>
+                    )
+                  }
+                </View>
+              </ScrollView>
+              </View>
+
+          {/* </View> */}
           </ScrollView>
         </KeyboardAvoidingView>
       <View></View>
@@ -153,10 +164,18 @@ function WriteScreen2({route,saveResult}) {
 
 const styles = StyleSheet.create({
  txt:{
-  top:-70,
+  textAlign:"center",
+  marginBottom:40,
+  marginLeft: 10,
+  marginright:10,
+  color:'white'
  },
  scrView:{
-  
+  paddingHorizontal:20,
+  width:360,
+  marginHorizontal:20,
+  paddingTop:10,
+  height:680,
 
  },
     block: {
@@ -165,8 +184,11 @@ const styles = StyleSheet.create({
         
       },
       imgview:{
-        top:-90,
-        // backgroundColor:"red",
+        top:0,
+        alignItems:"center",
+        marginTop: 15,
+        marginBottom:30,
+        //backgroundColor:"red",
       },
       editor:{
         width:360,
@@ -178,12 +200,15 @@ const styles = StyleSheet.create({
       tarot:{
         paddingHorizontal:20,
         alignItems:"center",
-        borderRadius:10,
+        borderRadius:35,
+        borderColor:'#F0C997',
+        borderWidth:1,
         width:360,
         marginHorizontal:30,
-        backgroundColor:"aliceblue",
-        paddingTop:30,
+        backgroundColor:"black",
+        paddingTop:10,
         height:680,
+        paddingBottom:15
       },
       
 });
